691a32a26b54135b22973e7812d9c1ff661c61e5
AnyaSE <annseleznova@gmail.com>
2022-12-28 09:50:38 +0800

    corrected js code for HW6

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 const array = [3, 5, 7, 10, 12, 14, 2, 6, 8];
 
 function removeElement (array, item) {
-    for (let i = 0; i < array.length; i++) {
-        const number = array[i];
-        if (array.includes(number)) {
-            array.splice(item, 1);
-        } else {
-        console.log(`This index is not in the array`);
-        } 
-        return array;
-    }
+    if (array.includes(item)) {
+        array.splice(item, 1);
+    } else {
+    console.log(`This number is not in the array`);
+    } 
+    return array;
 };
 
 console.log(removeElement(array, 7));
